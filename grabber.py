d04b742e11489df594d6b5d8e2ccac581c417c74
Assil Ksiksi <assilksiksi@gmail.com>
2012-11-20 17:17:35 +0400

    Grabber now tackles edge cases with labs

--- grabber.py
+++ grabber.py
@@ -2,6 +2,7 @@ import mechanize
 import cookielib
 import cPickle
 from bs4 import BeautifulSoup
+from collections import defaultdict
 
 def html_to_pickle(source):
     '''Collect data from HTML source and write it to a pickle.'''
@@ -19,22 +20,50 @@ def html_to_pickle(source):
     # Order course data by course abbreviation (ex: PHYS)
     courses_by_abbrev = {}
 
-    for course in courses:
+    for index, course in enumerate(courses):
         cols = [each.text for each in course.select('.dddefault')]
         
-        # Create dict for each valid section
+        # Create dict for each valid section and add to final dict
         if len(cols) == 17:
             info = {header:col for header, col in zip(headers, cols)}
-        
-        # Add course to course dict under correct abbrev, code, and crn
-        if info['abbrev'] not in courses_by_abbrev:
-            courses_by_abbrev[info['abbrev']] = {info['code']: {info['crn']: info}}
-        else:
-            current = courses_by_abbrev[info['abbrev']]
-            if info['code'] in current:
-                current[info['code']][info['crn']] = info
+            
+            # If there are 11 empty columns, check if this entry is related to previous entry; if it is, do some magic
+            if cols.count(u'\xa0') in [11, 12]:
+                # Find info of previous entry
+                previous_info = courses[index-1]
+
+                # Check if previous entry is indeed the correct parent
+                if previous_info.values().count(u'\xa0') not in [11, 12]:
+                    abbrev, code, crn, days = previous_info['abbrev'], previous_info['code'], previous_info['crn'], info['days']
+                    
+                    # Verify duration to make sure it's not a final exam date
+                    if info['duration'] == previous_info['duration']:
+                        
+                        # If the instructor is the same, simply add the second day to the original entry
+                        if info['instructor'] == previous_info['instructor']:
+                            courses_by_abbrev[abbrev][code][crn]['days'] += days # Append day to previous day
+                        # Otherwise, make it a lab section (see: ITBP 319)
+                        else:
+                            for key in info:
+                                # Fill up any empty info
+                                if info[key] == u'\xa0':
+                                    # Append a 'L' to section
+                                    if key == 'section' or key == 'crn':
+                                        info[key] = 'L' + previous_info[key]
+                                    else:
+                                        info[key] = previous_info[key]
+            
+            # Add course to course dict under correct abbrev, code, and crn
+            if info['abbrev'] not in courses_by_abbrev:
+                courses_by_abbrev[info['abbrev']] = {info['code']: {info['crn']: info}}
             else:
-                current[info['code']] = {info['crn']: info}
+                current = courses_by_abbrev[info['abbrev']]
+                if info['code'] in current:
+                    current[info['code']][info['crn']] = info
+                else:
+                    current[info['code']] = {info['crn']: info}
+
+            courses[index] = info
 
     # Write dict to pickle
     cPickle.dump(courses_by_abbrev, open('classes.pickle', 'wb'))
@@ -87,7 +116,7 @@ def source_grabber():
     # Get page source
     response = br.response()
     source = response.read()
-
+    
     return source
 
 def main():

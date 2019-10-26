260047fdabfb6b74a8501d2a4da6af1362931c5f
Home <htpc@assil.me>
2019-10-25 22:49:33 -0400

    Basically final version of new grabber

--- grabber_v2.py
+++ grabber_v2.py
@@ -1,15 +1,18 @@
 import asyncio
+import json
 import os
-import pprint
 import random
 import string
 import time
+from typing import List
 
 import aiohttp
 import requests
 
 
-UAEU_BASE_URL = "eservices.uaeu.ac.ae"
+BASE_URL = "eservices.uaeu.ac.ae"
+PAGE_SIZE = 50
+MAX_TIMEOUT = 10
 
 
 def get_session_id():
@@ -24,11 +27,11 @@ class CourseGrabber:
     """Grabs courses for one or more terms.
 
     Args:
-        loop: asyncio event loop
         base_url (str): Base Banner URL for university (e.g., eservices.uaeu.ac.ae)"
+        timeout (int): Max timeout for each request, in seconds
     """
-    def __init__(self, base_url):
-        self.session = None
+    def __init__(self, base_url: str=BASE_URL, timeout: int=MAX_TIMEOUT):
+        self.timeout = timeout
 
         # Build university-specific URLs
         self.terms_url = "https://%s/StudentRegistrationSsb/ssb/classSearch/getTerms" \
@@ -39,8 +42,7 @@ class CourseGrabber:
             "https://%s/StudentRegistrationSsb/ssb/classSearch/resetDataForm" % base_url
         self.subjects_url = \
             "https://%s/StudentRegistrationSsb/ssb/classSearch/get_subject" \
-            "?dataType=json&term={term}&offset=1&max=-1&uniqueSessionId=" \
-            "{session_id}" % base_url
+            "?dataType=json&term={term}&offset=1&max=-1" % base_url
         self.courses_url = \
             "https://%s/StudentRegistrationSsb/ssb/searchResults/searchResults" \
             "?txt_subject={subject}&txt_term={term}&startDatepicker=&endDatepi" \
@@ -50,50 +52,52 @@ class CourseGrabber:
     def get_terms(self):
         return [each["code"] for each in requests.get(self.terms_url).json()]
 
-    async def _get_subjects(self, term, client):
+    async def _get_subjects(self, term: str):
         """Get all subjects for a given term."""
-        args = {"session_id": self.session_id, "term": term}
+        args = {"term": term}
         url = self.subjects_url.format(**args)
 
-        async with client.get(url) as resp:
-            data = await resp.json()
-            print(data)
+        async with aiohttp.ClientSession() as client:
+            async with client.get(url) as resp:
+                data = await resp.json()
 
         return data
 
-    async def _get_courses_page(self, term, subject, session_id, client,
-                                page_offset=0, page_size=50):
+    async def _get_courses_page(self, term: str, subject: str, session_id: str,
+                                client: aiohttp.ClientSession,
+                                page_offset: int=0, page_size: int=PAGE_SIZE):
         args = {"session_id": session_id, "term": term, "subject": subject,
                 "page_offset": page_offset, "page_size": page_size}
         url = self.courses_url.format(**args)
 
-        async with client.get(url) as resp:
-            data = await resp.json()
+        try:
+            async with client.get(url, timeout=self.timeout) as resp:
+                data = await resp.json()
+        except Exception:
+            print(f"{subject}: timed out")
+            return ([], 0)
 
         if not data["success"]:
             return ([], 0)
 
         return (data["data"], data["totalCount"])
 
-    def _transform_courses(self, data, subject):
+    def _transform_courses(self, data, subject: str):
         # Transform list to dict of subject -> course number -> data
-        transformed = {}
+        transformed = {subject: {}}
 
         for info in data:
             course_number = info["courseNumber"]
             crn = info["courseReferenceNumber"]
 
-            if subject in transformed:
-                if course_number in transformed[subject]:
-                    transformed[subject][course_number][crn] = info
-                else:
-                    transformed[subject][course_number] = {crn: info}
+            if course_number in transformed[subject]:
+                transformed[subject][course_number][crn] = info
             else:
-                transformed[subject] = {course_number: {crn: info}}
+                transformed[subject][course_number] = {crn: info}
 
         return transformed
 
-    async def _get_courses(self, term, subject):
+    async def _get_courses(self, term: str, subject: str):
         session_id = get_session_id()
         payload = {
             "uniqueSessionId": session_id,
@@ -104,8 +108,14 @@ class CourseGrabber:
         client = aiohttp.ClientSession()
 
         # Start new search session
-        async with client.post(self.start_search_url, data=payload) as resp:
-            data = await resp.json()
+        try:
+            async with client.post(self.start_search_url, data=payload,
+                                   timeout=self.timeout) as resp:
+                data = await resp.json()
+        except Exception:
+            print(f"{subject}: search timed out")
+            await client.close()
+            return {subject: {}}
 
         # Grab first page of course results
         data, total = \
@@ -138,33 +148,54 @@ class CourseGrabber:
 
         return transformed
 
-    async def _search(self, term, subjects):
-        """Start a new Banner course search for a given term."""
+    async def search(self, term: str, subject: str):
+        """Start a new Banner course search for a given term and subject."""
+        print(f"Starting {subject}")
+        result = await self._get_courses(term, subject)
+        print(f"Done with {subject}")
+        return result
+
+    async def fetch(self, term: str):
+        """Fetch all courses in a given term."""
         # Get all subjects for this term
-        if not subjects:
-            async with aiohttp.ClientSession() as client:
-                subjects = [each["code"] for each in
-                            await self._get_subjects(term, client)]
+        subjects = [each["code"] for each in
+                    await self._get_subjects(term)]
 
-        tasks = [self._get_courses(term, subject) for subject in subjects]
+        print(f"{term}: number of subjects is {len(subjects)})"
 
-        # Get course data for all subjects in the term
-        results = [res for res in await asyncio.gather(*tasks)]
+        tasks = [self.search(term, subject) for subject in subjects]
 
-        return results
-
-    async def fetch(self, terms, subjects=[]):
-        """Start searching for courses in one or more terms."""
-        tasks = [self._search(term, subjects) for term in terms]
+        print(f"{term}: started")
         results = await asyncio.gather(*tasks)
+        print(f"{term}: completed")
+
         return results
+
+    async def fetch_all(self, terms: List[str]):
+        """Fetch all courses for one or more terms."""
+        tasks = [self.fetch(term) for term in terms]
+        return await asyncio.gather(*tasks)
+
 
 def main():
-    grabber = CourseGrabber(base_url=UAEU_BASE_URL)
-    terms = grabber.get_terms()
-    output = asyncio.run(grabber.fetch(["202010"], ["MATH", "ELEC", "MECH"]))
+    grabber = CourseGrabber()
 
-    #print(output)
+    # Grab all course data for latest 3 terms
+    terms = grabber.get_terms()[:3]
+    results = asyncio.run(grabber.fetch_all(terms))
+
+    for term, output in zip(terms, results):
+        course_data = {}
+
+        for each in output:
+            subject = list(each.keys())[0]
+            data = each[subject]
+
+            course_data[subject] = data
+
+        with open(f"/tmp/courses-{term}.json", "w") as f:
+            json.dump(course_data, f)
+
 
 if __name__ == "__main__":
     main()
